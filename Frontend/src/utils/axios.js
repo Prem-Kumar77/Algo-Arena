e1ccb831b671706a97ccc1028dacd4e78604e3d7
Prem-Kumar77 <kuruvapremkumar7@gmail.com>
2025-10-17 18:10:34 +0530

    changes to axios

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://algo-arena-eepm-prem-kumars-projects-2383c494.vercel.app/api",
   withCredentials: true,
 });
 
